498a6993f86a2b293400c57a9eed3210004e3647
Tommy <lathu@belive.sg>
2022-06-26 10:33:34 +0700

    - Update SPM name

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "TLSegmentedControl-iOS",
+    name: "TLSegmentedControl",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TLSegmentedControl-iOS",
-            targets: ["TLSegmentedControl-iOS"]),
+            name: "TLSegmentedControl_iOS",
+            targets: ["TLSegmentedControl_iOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "TLSegmentedControl-iOS",
+        .target(name: "TLSegmentedControl_iOS",
                 path: "Classes",
                 linkerSettings: [
                     .linkedFramework("UIKit", .when(platforms: [.iOS]))
